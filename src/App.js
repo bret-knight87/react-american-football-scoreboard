b907b5a47364d2aa911d984a5e0276e16657b21a
bret-knight87 <bret.knight87@gmail.com>
2019-11-13 22:02:29 -0600

    made all counters functional

--- src/App.js
+++ src/App.js
@@ -7,6 +7,35 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [awayScore, setAwayScore] = useState(0);
   const [homeScore, setHomeScore] = useState(0);
+  const [outs, setOuts] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [innings, setInnings] = useState(0);
+
+  const outCounter = () => {
+    if (outs === 3) {
+      setOuts(0);
+    } else {
+      setOuts(outs + 1);
+    }
+  };
+
+  const strikeCounter = () => {
+    if (strikes === 3) {
+      setStrikes(0);
+    } else {
+      setStrikes(strikes + 1);
+    }
+  };
+
+  const ballsCounter = () => {
+    if (balls === 4) {
+      setBalls(0);
+    } else {
+      setBalls(balls + 1);
+    }
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -27,19 +56,34 @@ function App() {
         <div class="bottomRow">
           <div class="down">
             <h3 class="down__title">Inning</h3>
-            <div class="down__value">7</div>
+            <div class="down__value">{innings}</div>
+            <button
+              className="homeButtons__touchdown"
+              onClick={() => setInnings(innings + 1)}>
+              Inning
+            </button>
           </div>
+
           <div class="toGo">
             <h3 class="toGo__title">Outs</h3>
-            <div class="toGo__value">1</div>
+            <div class="toGo__value">{outs}</div>
+            <button className="homeButtons__touchdown" onClick={outCounter}>
+              Outs
+            </button>
           </div>
           <div class="ballOn">
             <h3 class="ballOn__title">Balls</h3>
-            <div class="ballOn__value">3</div>
+            <div class="ballOn__value">{balls}</div>
+            <button className="homeButtons__touchdown" onClick={ballsCounter}>
+              Ball
+            </button>
           </div>
           <div class="quarter">
             <h3 class="quarter__title">Strikes</h3>
-            <div class="quarter__value">2</div>
+            <div class="quarter__value">{strikes}</div>
+            <button className="homeButtons__touchdown" onClick={strikeCounter}>
+              Strike
+            </button>
           </div>
         </div>
       </section>
@@ -48,21 +92,27 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button
             className="homeButtons__touchdown"
-            onClick={() => setHomeScore(homeScore + 1)}
-          >
+            onClick={() => setHomeScore(homeScore + 1)}>
             Run Scored
           </button>
-          <button className="homeButtons__touchdown" onClick={() => setHomeScore(0)}>Reset</button>
+          <button
+            className="homeButtons__touchdown"
+            onClick={() => setHomeScore(0)}>
+            Reset
+          </button>
           {/* <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button> */}
         </div>
         <div className="awayButtons">
           <button
             className="awayButtons__touchdown"
-            onClick={() => setAwayScore(awayScore + 1)}
-          >
+            onClick={() => setAwayScore(awayScore + 1)}>
             Run Scored
           </button>
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(0)}>Reset</button>
+          <button
+            className="awayButtons__touchdown"
+            onClick={() => setAwayScore(0)}>
+            Reset
+          </button>
           {/* <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button> */}
         </div>
       </section>
