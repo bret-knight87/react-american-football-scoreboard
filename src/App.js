8f09c86747a14d6743f497f497f95ed7bd310c4c
bret-knight87 <bret.knight87@gmail.com>
2019-11-13 22:05:27 -0600

    added bg color to teams

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Cards</h2>
+            <h2 className="home__name">Cubs</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -49,7 +49,7 @@ function App() {
           </div>
           {/* <div className="timer">00:03</div> */}
           <div className="away">
-            <h2 className="away__name">Cubs</h2>
+            <h2 className="away__name">Cards</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
